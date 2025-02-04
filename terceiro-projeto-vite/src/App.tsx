fe5b6f3de7e8e438c09db79b3cf32c1a46a9065f
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-02-04 20:59:17 -0300

    lesson 55
    styled components pseudoselectors and nesting

--- terceiro-projeto-vite/src/App.tsx
+++ terceiro-projeto-vite/src/App.tsx
@@ -1,27 +1,9 @@
-/*
-//criamos um componente já com o estilo dele
-const Card = styled.div`
-  background-color: white;
-  width: 400px;
-  height: 400px;
-  padding: 30px;
-  color: blue;
-`
-  Para organizar o codigo podemos criar um arquivo index.tsc e colocar esse componente lá
-
-*/
-
-import Card from "./components/Card";
+import { Input } from "./components/input";
 
 const App = () => {
-  //importa o Card.tsx do component e usa na aplicaçao
   return(
-    <Card/>
-  )
+      <Input></Input>
+    )
 }
 
 export default App;
-
-//resumo: criamos um arquivo de css com o componente que exporta para Card.tsx
-//Card.tsx exporta default - que vai para App.tsx
-//Em App.tsx importamos o Card da pasta components 
---
+++ terceiro-projeto-vite/src/components/input.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    padding: 10px 30px;
+    outline: none;
+    transition: all .6s;
+    border: 1px solid transparent;
+
+    &:focus{
+        border-color: red;
+    }
+
+    &:hover{
+        background-color: hotpink;
+    }
+`
