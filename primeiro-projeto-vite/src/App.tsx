d4cd71631c981793d1bcfbbedd4e3d6cbccc0b60
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-12 21:27:09 -0300

    if com operador logico &&

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -3,18 +3,13 @@ import { UserInfo } from "./components/UserInfo";
 const App = () => {
   const isLogged = true;
 
-  if(isLogged){
-    return(
-      <div>
-        <h1>Componente de um usuário</h1>
-        <UserInfo/>
-      </div>
-    )
-  } 
-
   return(
-    <h1>Não esta logado</h1>
+    <div>
+      <h1>Componente de um usuário</h1>
+      {isLogged && <UserInfo/>}
+    </div>
   )
+  
 }
 
 export default App;
