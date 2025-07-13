3bdd5b8e67801a3d5e11ffd515a53372126e2c0b
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-12 21:32:10 -0300

    renderização condicional com operador ternário

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -1,11 +1,18 @@
 import { UserInfo } from "./components/UserInfo";
 
 const App = () => {
-  const isLogged = true;
+  const isLogged = false;
+
+  //se nao esta logado o return interrompe o resto do código
+  if(!isLogged) {
+    return;
+  }
 
   return(
     <div>
-      <h1>Componente de um usuário</h1>
+      <h1>
+        {isLogged ? "Logado" : "Não está logado"}
+      </h1>
       {isLogged && <UserInfo/>}
     </div>
   )
