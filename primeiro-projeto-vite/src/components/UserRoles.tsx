3d23ae7d55dcf76aa2e20eb18032b8a39ddf4eb2
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-12 21:42:26 -0300

    renderizando listas de objetos com id

--- primeiro-projeto-vite/src/components/UserRoles.tsx
+++ primeiro-projeto-vite/src/components/UserRoles.tsx
@@ -1,11 +1,15 @@
 export const UserRoles = () => {
-    const roles = ['CEO', 'CTO','admin'];
+    const roles = [
+        {id: 1, title: 'CEO'}, 
+        {id: 2, title: 'CTO'}, 
+        {id: 3, title: 'admin'}
+    ];
     
     return(
         <ul>
-            {roles.map((value, key) => (
-                <li key={key}>
-                    {value}
+            {roles.map(value => (
+                <li key={value.id}>
+                    {value.title}
                 </li>
             ))}
         </ul>
